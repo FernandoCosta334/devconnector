0e203b71f75eab172a5fe3241ba9b6447e715d27
Fernando <fc-nardi@hotmail.com>
2018-12-04 23:49:46 -0300

    Adicionado routes

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://admin:f1e0r8n0@ds121624.mlab.com:21624/devconnector"
+  mongoURI: "mongodb://admin:f1e0r8n0@ds121624.mlab.com:21624/devconnector",
+  secretOrKey: "secret"
 };
