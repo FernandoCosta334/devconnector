97d67ef2e82d7ece26edff7eecf74cf23118534e
FernandoCostaHs <fc-nardi@hotmail.com>
2019-01-10 09:07:44 -0200

    adicionado register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Register extends Component {
   constructor() {
@@ -19,6 +20,22 @@ class Register extends Component {
     this.setState({ [e.target.name]: e.target.value });
   }
 
+  onSubmit(e) {
+    e.preventDefault();
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    };
+
+    axios
+      .post("/api/users/register", newUser)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err.response.data));
+  }
+
   render() {
     return (
       <div className="register">
@@ -26,7 +43,9 @@ class Register extends Component {
           <div className="row">
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Sign Up</h1>
-              <p className="lead text-center">Create your DevConnector account</p>
+              <p className="lead text-center">
+                Create your DevConnector account
+              </p>
               <form onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input
@@ -48,7 +67,8 @@ class Register extends Component {
                     onChange={this.onChange}
                   />
                   <small className="form-text text-muted">
-                    This site uses Gravatar so if you want a profile image, use a Gravatar email
+                    This site uses Gravatar so if you want a profile image, use
+                    a Gravatar email
                   </small>
                 </div>
                 <div className="form-group">
